1f882a7bc6fec022fbd476da8e1c20377c54792f
aaronquan <z5020387@zmail.unsw.edu.au>
2016-03-14 14:59:01 +1100

    Update api testers

--- api/rest/messages.js
+++ api/rest/messages.js
@@ -2,19 +2,36 @@ var express = require('express');
 
 var app = express();
 var messages = '{ "messages": [' +
-				'"Hello world!",' +
-				'"Hopefully this works",' +
-				'"Goodnight" ]}';
+				'{"m":"Hello world!", "sentBy": 1, "sentTo": 0},' +
+				'{"m":"Hopefully this works", "sentBy": 0, "sentTo": 1},' +
+				'{"m":"Goodnight", "sentBy": 0, "sentTo": 1} ]}';
+
+var people = '{ "people": [' +
+			'{"firstName":"Mike", "lastName":"Smith"},' +
+			'{"firstName":"John", "lastName":"Wong"} ]}'; 
 
 messages = JSON.parse(messages);
+people = JSON.parse(people);
+app.set('json spaces', 20);
+
 
 app.get(/messages\/[0-9]*/, function(req, res){
+	//res.setHeader('Content-Type', 'application/json');
 	var num = req.url.replace(/\/messages\//, '');
 	num = parseInt(num);
-	res.send(messages.messages[num]);
+	res.json(messages.messages[num]);
+});
+app.get(/people\/[0-9]*/, function(req, res){
+	var num = req.url.replace(/\/people\//, '');
+	num = parseInt(num);
+	res.json(people.people[num]);
 });
 app.get(/messages/, function(req, res){
-	res.send(messages);
+	//res.setHeader('Content-Type', 'application/json');
+	res.json(messages);
+});
+app.get(/people/, function(req, res){
+	res.json(people);
 });
 app.get(/[a-z]*/, function(req, res){
 	res.send("An API request for messages: request /messages. "+
--- api/rest/routes/api.js
+++ api/rest/routes/api.js
@@ -6,11 +6,8 @@ var Product = require('../models/product');
 
 // Routes
 Product.methods(['get', 'put', 'post', 'delete']);
-Product.register(router, '/products')
+Product.register(router, '/products');
 
-router.get('/products', function(req, res){
-	res.send('working')
-});
 
 //return router
 module.exports = router;
