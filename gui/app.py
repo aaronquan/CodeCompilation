4d43cfa9ab0e3cf035eceaf64b0e1803f88e9f7b
aaronquan <z5020387@zmail.unsw.edu.au>
2016-05-05 13:17:58 +1000

    gui experiments

--- gui/app.py
+++ gui/app.py
@@ -1,8 +1,10 @@
 from tkinter import *
 
 def main():
-	root = App(Tk())
-	root.tk.mainloop()
+	root = Tk()
+	root.geometry("400x400")
+	app = App(root)
+	root.mainloop()
 
 def hello():
     print ("hello!")
@@ -12,21 +14,37 @@ def hello():
 class App():
 	def __init__(self, master=None):
 		self.tk = master
+		self.createCanvas()
 		self.menuBar()
+		self.events()
+		self.buttons()
 
 	def menuBar(self):
 		menubar = Menu(self.tk)
 		filemenu = Menu(menubar, tearoff=0)
-		filemenu.add_command(label="New", command=self.canvas)
+		filemenu.add_command(label="New", command=self.createCanvas)
 		filemenu.add_command(label="Open", command=hello)
 		filemenu.add_command(label="Save", command=hello)
 		filemenu.add_separator()
 		filemenu.add_command(label="Exit", command=self.tk.quit)
 		menubar.add_cascade(label="File", menu=filemenu)
 		self.tk.config(menu=menubar)
-	def canvas(self):
-		canvas = Canvas(self.tk, bg='#333399')
-		canvas.create_bitmap(100,100)
-		canvas.pack()
+	def buttons(self):
+		f = Frame(self.tk, bg='#123456')
+		closeButton = Button(f, text="Close")
+		closeButton.pack(side=RIGHT, padx=5, pady=5)
+		okButton = Button(f, text="OK")
+		okButton.pack(side=RIGHT)
+		f.place(x=0, y=0)
+
+	def createCanvas(self):
+		c = Canvas(self.tk, width=300, height=300, bg='#111111')
+		c.pack()
+		self.canvas = c
+	def events(self):
+		self.tk.bind_class('Canvas','<Button-1>', self.click)
+	def click(self, event):
+		self.canvas.create_rectangle(event.x, event.y, event.x, event.y, outline='green')
+		print ("Clicked: "+str(event.x)+":"+str(event.y))
 
 main()
